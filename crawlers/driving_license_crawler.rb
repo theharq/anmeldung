218911bfc6ebef5e2694035bba6d857b39ea785d
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2021-11-28 22:14:51 +0100

    Add new crawler

---
+++ crawlers/driving_license_crawler.rb
@@ -0,0 +1,22 @@
+require "capybara/dsl"
+require "capybara/apparition"
+
+Capybara.run_server = false
+Capybara.current_driver = :apparition
+Capybara.app_host = "https://service.berlin.de/dienstleistung/327537/"
+Capybara.default_max_wait_time = 15
+
+class DrivingLicenseCrawler
+  include Capybara::DSL
+
+  # To get these ids go to https://service.berlin.de/dienstleistung/327537/ and pick your district
+  BURGERAMT_IDS = ENV.fetch("BURGERAMT_IDS")
+
+  def initialize
+    visit("https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=327537&dienstleisterlist=#{BURGERAMT_IDS}")
+  end
+
+  def available_dates
+    all("td.buchbar a").map(&:text).map(&:to_i)
+  end
+end
